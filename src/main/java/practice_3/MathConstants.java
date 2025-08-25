7ee4a88e9a38cefdb6813a47aca36908f9f981a7
MA-Nepomnyaschikh <udv92LNWv5NJgNU>
2025-08-25 21:26:53 +0300

    Домашнее задание по уроку "Static, final и модификаторы доступа"

---
+++ src/main/java/practice_3/MathConstants.java
@@ -0,0 +1,14 @@
+package main.java.practice_3;
+
+public class MathConstants {
+    public static final double PI = 3.14159;
+    public static final double E = 2.71828;
+
+    public static double calculateCircleArea(double radius) {
+        return PI * Math.pow(radius, 2);
+    }
+
+    public static double calculateCircumference(double radius) {
+        return 2 * PI * radius;
+    }
+}
